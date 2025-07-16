1a50ed6ac8029dab436d2eeece79ab8408673a75
DennisTurco <dennisturco@gmail.com>
2025-07-16 10:14:56 +0200

    ignore target folder and website moved to root folder

--- target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.dennisturco.exceptionpopupmessage","l":"ExceptionDialogListener"},{"p":"com.dennisturco.exceptionpopupmessage","l":"ExceptionPopupMessage"}];updateSearchResults();
